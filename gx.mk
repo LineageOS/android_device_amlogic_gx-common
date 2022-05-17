ee688f7cfdf6e0f14ee4ded19a692594dadc7916
Vitaliy Tomin <highwaystar.ru@gmail.com>
2022-05-17 22:49:59 +0800

    gx: Uprev audio
    Change-Id: Ie641f2bdec7a946f50a06f7db89aff661320153f

--- gx.mk
+++ gx.mk
@@ -27,8 +27,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@4.0-impl \
+    android.hardware.audio@4.0 \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.hearing_aid.default \
