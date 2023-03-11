9e2ed2b55d1a7e1d5d365a9038a88b9655ab1dd2
Vitaliy Tomin <highwaystar.ru@gmail.com>
2023-03-11 16:13:05 +0800

    gx-common: Add proprietary DroidTvSettings app
    * this app allow to set screen resolution and position
    * from kvim-userdebug 9 PPR1.180610.011 20211022 test-keys
    
    Change-Id: Id29aa09b69955b9c9e3c5d0aa77106cfe0d95fd7

--- gx.mk
+++ gx.mk
@@ -89,6 +89,10 @@ PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+## Permissions privapp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp_allowlist_com.droidlogic.tv.settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp_allowlist_com.droidlogic.tv.settings.xml
+
 ## Platform
 TARGET_AMLOGIC_SOC ?= gxl
 
