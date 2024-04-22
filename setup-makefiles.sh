8ae1d8dead298762cb7e3707aa74b08718bf0192
Nolen Johnson <johnsonnolen@gmail.com>
2024-04-22 18:19:06 -0400

    gx: Add g12b to the guards
    Change-Id: I478e1e3f35d5d8a24000cd4fcff4a2e5b0a3cf16
    (cherry picked from commit 3b96afbde2a446c38adb5bf2ef277a0769441903)

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "gxm gxl" "TARGET_AMLOGIC_SOC"
+write_headers "gxm gxl g12b" "TARGET_AMLOGIC_SOC"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
