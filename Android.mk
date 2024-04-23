619b0344d1f9b2e02b42b1fba044e3dc27bfa85d
Nolen Johnson <johnsonnolen@gmail.com>
2024-04-22 20:08:22 -0400

    gx: Allow some SOCs to fall back to gx tree
    Change-Id: If061f6b0feea830e8e497d18630fc4e428388d52
    (cherry picked from commit 8153cdd48eb509aded0f1f4c61a4950ca87ffe9f)

--- Android.mk
+++ Android.mk
@@ -4,9 +4,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ifneq ($(filter gxm gxl g12b,$(TARGET_AMLOGIC_SOC)),)
+ifneq ($(filter gxm gxl,$(TARGET_AMLOGIC_SOC)),)
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+endif
+
+ifneq ($(filter g12b,$(TARGET_AMLOGIC_SOC)),)
+ifeq ($(TARGET_USES_P_MODULES),true)
+LOCAL_PATH := $(call my-dir)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
 endif
