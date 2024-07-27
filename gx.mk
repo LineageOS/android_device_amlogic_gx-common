b42376f869aa33168759a0ff5cff5d8123f5f40a
Bruno Martins <bgcngm@gmail.com>
2024-07-27 09:38:48 +0100

    gx: Drop AOSP default health HAL
    The custom batteryless HAL is now built for all amlogic targets.
    
    Change-Id: I9208ca017de0783701a04d44692d50d81b248d30

--- gx.mk
+++ gx.mk
@@ -53,10 +53,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/mesondisplay.cfg:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/mesondisplay.cfg \
     $(LOCAL_PATH)/configs/mesondisplay.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mesondisplay.cfg
 
-## Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
-
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport \
