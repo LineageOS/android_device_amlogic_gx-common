985f20a00af54772e09e8335c5dd4a7ff836c693
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-08 14:51:59 -0500

    gx: Don't duplicate entry for fstab
    Change-Id: I47d3a4c20698312c43fe83cf02e5eefb9965d691

--- gx.mk
+++ gx.mk
@@ -69,9 +69,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.usb.rc \
     $(LOCAL_PATH)/init-files/init.recovery.amlogic.rc:recovery/root/init.recovery.amlogic.rc
 
-PRODUCT_PACKAGES += \
-    fstab.amlogic
-
 ## Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
