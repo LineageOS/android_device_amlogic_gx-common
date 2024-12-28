b5081e0c18a07e179acfce0af71961620bf4e5d5
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-27 23:25:12 -0500

    gx: Migrate back to Android.mk handling for kernel modules
    * These modules are old, not worth fixing, and gx dies on 20/21 anyway.
    
    Change-Id: Ide3a5647524ffbd061cd7eadad371eea3f64362b

--- gx.mk
+++ gx.mk
@@ -69,6 +69,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.usb.rc \
     $(LOCAL_PATH)/init-files/init.recovery.amlogic.rc:recovery/root/init.recovery.amlogic.rc
 
+## Kernel Modules
+PRODUCT_PACKAGES += \
+    mali \
+    media \
+    optee-module
+
 ## Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-service \
